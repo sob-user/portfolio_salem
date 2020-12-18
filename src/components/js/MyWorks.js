d1da60d6d349d2ab157cc79f678f134758383939
sob-user <oulad.b.s@gmail.com>
2020-12-18 17:28:40 +0100

    delete old project and new projects

--- src/components/js/MyWorks.js
+++ src/components/js/MyWorks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import olderLogo from '../img/logo_older.png'
-import parkAssitLogo from '../img/logo_park_assist.png'
+import parkAssitLogo from '../img/logo_javascript30.png'
+import pikachu from '../img/pokemon.png'
 import '../css/MyWorks.css'
 
 export class MyWorks extends Component {
@@ -34,24 +35,37 @@ export class MyWorks extends Component {
                     </div>
                     <div className='MyWorksItem'>
                         <img src={parkAssitLogo} alt='park assit game logo'></img>
-                            <div className='MyWorksDescription'>
-                            it's a game that simulates the parking assistance system.
-                            </div>
+                            <div className='MyWorksDescription'>My solutions for the #<a href='https://javascript30.com' target='_blank' rel='noopener noreferrer'>Wes Bos challenge</a>#</div>
                             <div className='MyWorksItemLink'>
                             <div className='MyWorksLink'>
-                                {/* <a href='https://older-stream.com' target='_blank' rel='noopener noreferrer'> */}
-                                in production
-                                {/* </a> */}
+                                <a href='https://sob-user.github.io/javascript30_mySolutions/' target='_blank' rel='noopener noreferrer'>
+                                view project
+                                </a>
                             </div>
                             <div className='MyWorksLink'>
-                                <a href='https://github.com/sob-user/park_assist_js' target='_blank' rel='noopener noreferrer'>
+                                <a href='https://github.com/sob-user/javascript30_mySolutions' target='_blank' rel='noopener noreferrer'>
                                     view code
                                 </a>
                             </div>
                         </div>
                     </div>
                     <div className='MyWorksItem'>
-                        Work in progress
+                        <img src={pikachu} alt='older app logo'></img>
+                                <div className='MyWorksDescription'>
+                                Pokédex using pokeAPI
+                                </div>
+                        <div className='MyWorksItemLink'>
+                            <div className='MyWorksLink'>
+                                <a href='https://sob-user.github.io/pokedexAPI_mySolution/' target='_blank' rel='noopener noreferrer'>
+                                    view project
+                                </a>
+                            </div>
+                            <div className='MyWorksLink'>
+                                <a href='https://github.com/sob-user/pokedexAPI_mySolution' target='_blank' rel='noopener noreferrer'>
+                                    view code
+                                </a>
+                            </div>
+                        </div>
                     </div>
                     <div className='MyWorksItem'>
                         Work in progress
